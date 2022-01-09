622fe2eb6ae99e6fdfe1f7e1652691a03078c3b0
Ian Gregory <justcheesy@outlook.com>
2022-01-08 22:24:03 -0500

    Fix style errors caught by brew test-bot

--- Casks/openstore.rb
+++ Casks/openstore.rb
@@ -1,10 +1,10 @@
 cask "openstore" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
-  
+
   version "0.1.0"
 
   url "https://github.com/GetOpenStore/OpenStore/releases/download/#{version}/OpenStore-darwin-#{arch}-#{version}.zip",
-    verified: "https://github.com/GetOpenStore/OpenStore"
+      verified: "https://github.com/GetOpenStore/OpenStore"
   if Hardware::CPU.intel?
     sha256 "e1ffb2d28da9e6bbad567ec100af91c2dd3c487ee8c368cfc40c1103c4238576"
   else
@@ -12,7 +12,7 @@ cask "openstore" do
   end
 
   name "OpenStore"
-  desc "An open app store"
+  desc "Open app store"
   homepage "https://getopenstore.app/"
 
   livecheck do

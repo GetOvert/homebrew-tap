82393a0662c986e590e2be7716dad29e90abce7c
Ian Gregory <justcheesy@outlook.com>
2022-02-21 17:09:44 -0500

    Bump version to 0.1.4

--- Casks/openstore.rb
+++ Casks/openstore.rb
@@ -1,14 +1,14 @@
 cask "openstore" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "0.1.3"
+  version "0.1.4"
 
   url "https://github.com/GetOpenStore/OpenStore/releases/download/v#{version}/OpenStore-darwin-#{arch}-#{version}.zip",
       verified: "https://github.com/GetOpenStore/OpenStore"
   if Hardware::CPU.intel?
-    sha256 "c0602e6ccc924ec5bffff5b2f77d91aa38a983d00a5688025692b4aaf171e181"
+    sha256 "105cd41776d4cd099afcff98070fcc5c6db81b9fefdb847ace53ab260d062c46"
   else
-    sha256 "2e5d9f7e5ecd64401065189e333ea70be8ec7d5c3db7a5cfa548ebbb3aca7f4b"
+    sha256 "6e597436e88a1daf14141886689dd016787b9c377540acc523ed69e76c9374ea"
   end
 
   name "OpenStore"
@@ -25,6 +25,7 @@ cask "openstore" do
   zap trash: [
     "~/Library/Application Support/OpenStore",
     "~/Library/Caches/OpenStore_v1",
+    "~/Library/Caches/OpenStore_v2",
     "~/Library/Preferences/app.getopenstore.OpenStore.plist",
   ]
 end

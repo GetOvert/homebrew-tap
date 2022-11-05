de2fff91f136f34c6018322db31808ed65d3f329
Ian Gregory <justcheesy@outlook.com>
2022-11-05 16:42:29 -0400

    Update cask to use `arch` stanza

--- Casks/overt.rb
+++ Casks/overt.rb
@@ -1,16 +1,12 @@
 cask "overt" do
-  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  arch arm: "arm64", intel: "x64"
 
   version "0.4.0"
+  sha256 arm:   "8dbfd13f1f4ce89e78c2c5ba13102df41c88bf6c81d183ac34fa92c98d08d14f",
+         intel: "b9c6cd29b468dd741ef725a3479f74e6383e5a5c68195ba9f79809f5d30a0e2d"
 
   url "https://github.com/GetOvert/Overt/releases/download/v#{version}/Overt-darwin-#{arch}-#{version}.zip",
       verified: "https://github.com/GetOvert/Overt"
-  if Hardware::CPU.intel?
-    sha256 "b9c6cd29b468dd741ef725a3479f74e6383e5a5c68195ba9f79809f5d30a0e2d"
-  else
-    sha256 "8dbfd13f1f4ce89e78c2c5ba13102df41c88bf6c81d183ac34fa92c98d08d14f"
-  end
-
   name "Overt"
   desc "Open app store"
   homepage "https://getovert.app/"
